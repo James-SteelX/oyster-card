89db72a866c0e1970eb36a94597b104050edcd31
James <James.Steel@me.com>
2016-10-03 17:55:00 +0100

    last day 1 commit

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -4,8 +4,8 @@ require 'oyster_card'
 
 describe Journey do
 
-	let(:station) { double(:station, :zone => 1) }
-	let(:exit_station) { double(:station, :zone => 2)}
+	let(:station) { double(:station, :name => :station, :zone => 1) }
+	let(:exit_station) { double(:station, :name => :exit_station, :zone => 2)}
 
 	describe '#initialize' do
 
@@ -22,10 +22,22 @@ describe Journey do
 	end
 
 	describe '#calculate_fare' do
-		it 'should return a number greater than 0' do		
-			subject.entry_station = station
-			subject.exit_station = exit_station
-			expect(subject.fare).to be > 0
+		it 'should return a number greater than 0' do
+			card = OysterCard.new
+			station = Station.new(:entry)
+			exit_station = Station.new(:exit)
+			card.top_up(23)
+			card.touch_in(station)
+			card.touch_out(exit_station)
+			expect((station.zone - exit_station.zone).abs).to be > 0
+		end
+
+		it 'should return the penalty fare if you don\'t touch out' do
+			card = OysterCard.new
+			station = Station.new(:entry)
+			card.top_up(21)
+			card.touch_in(station)
+			expect(subject.calculate_fare).to eq 6
 		end
 	end
 end
